406c52d9830f83e956f53a478edafe5563fdeff4
blaisekwizera31-hash <blaisekwizera31@gmail.com>
2026-01-22 08:23:55 +0200

    c

--- src/ListGroup.tsx
+++ src/ListGroup.tsx
@@ -8,7 +8,7 @@ function ListGroup() {
     <>
       <h1>List</h1>
       {/* {items.length === 0? <p>No item found</p> : null} */}
-      <ul className="listgroup">
+      <ul className="list-group">
         {items.map((item, index) => (
           <li
             className={
