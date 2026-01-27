a4767e057f45961f52d9179c35d233dd2de69d98
blaisekwizera31-hash <blaisekwizera31@gmail.com>
2026-01-27 19:48:25 +0200

    commit

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,11 +1,11 @@
-import React from "react";
+
 
 interface Props {
   children: string;
   onClick?: () => void;
   color : string;
 }
-function Button({children, onClick, color}: Props) {
+function Button({children, onClick, color=' secondary'}: Props) {
   return <button className={`btn btn-${color}`} onClick = {
     onClick
   }>{children}</button>;
