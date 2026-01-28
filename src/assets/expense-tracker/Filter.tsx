3cf5812e96d12241027496ac180e0fa02c22b5f7
blaisekwizera31-hash <blaisekwizera31@gmail.com>
2026-01-28 16:23:20 +0200

    commit

--- src/assets/expense-tracker/Filter.tsx
+++ src/assets/expense-tracker/Filter.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Filter = () => {
   return (
-    <select className="form-select"></select>
+    <select className="form-select">
+        <option value="">All </option>
+        <option value="Groceries">Groceries</option>
+        <option value="Utilities">Utilities</option>
+        <option value="Entertainment">Entertainment</option>
+    </select>
   )
 }
 
