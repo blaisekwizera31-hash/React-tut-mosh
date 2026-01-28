bffe964e18aae10e1eb608bd6e88fbc90a5d9942
blaisekwizera31-hash <blaisekwizera31@gmail.com>
2026-01-28 08:08:16 +0200

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import ListGroup from "./ListGroup";
+import { FaUserMinus } from "react-icons/fa";
 
 function App() {
   return(
+    <>
+    <FaUserMinus color="red" size={40} />
     <div>
     <ListGroup />
   </div>
+  </>
   )
 }
   
--- src/ListGroup.tsx
+++ src/ListGroup.tsx
@@ -1,15 +1,10 @@
 import styles from './ListGroup.module.css'
 
+
 function ListGroup() {
   return (
     <>
-      <ul className={styles['list-group']}>
-        <li className={`${styles['list-group-item']} ${styles['container']}`}>An item</li>
-        <li className={`${styles['list-group-item']} ${styles['container']}`}>A second item</li>
-        <li className={`${styles['list-group-item']} ${styles['container']}`}>A third item</li>
-        <li className={`${styles['list-group-item']} ${styles['container']}`}>A fourth item</li>
-        <li className={`${styles['list-group-item']} ${styles['container']}`}>And a fifth one</li>
-      </ul>
+    
     </>
   );
 }
