3f4998e51f96d79c40a034221e710b4a2f5d1f57
blaisekwizera31-hash <blaisekwizera31@gmail.com>
2026-01-30 14:06:24 +0200

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,4 @@
-// // import Form from "./Form";
 
-// // function App() {
-// //   return(
-// //     <>
-// //     <Form></Form>
-// //   </>
-// //   )
-// // }
-
-// // export default App;
-
-// import { useState } from "react";
-// import ExpenseList from "./assets/expense-tracker/components/expenseList";
-// import Form from "./Form";
-// import Filter from "./assets/expense-tracker/Filter";
-// export const categories = ["Utilities", "Groceries", "Entertainment"];
-
-// const App = () => {
-//   const [expenses, setExpenses] = useState([
-//     {
-//       id: 1,
-//       description: "aaa",
-//       amount: 100,
-//       category: categories[0],
-//     },
-//     {
-//       id: 2,
-//       description: "bbb",
-//       amount: 200,
-//       category: categories[1],
-//     },
-//     {
-//       id: 3,
-//       description: "ccc",
-//       amount: 300,
-//       category: "Utilities",
-//     },
-//     {
-//       id: 4,
-//       description: "ddd",
-//       amount: 400,
-//       category: "Utilities",
-//     },
-//   ]);
-//   return (
-//     <>
-//       <div>
-//         <Form></Form>
-//       </div>
-//       <div>
-//         <Filter onSelectCategory={(category) => console.log(category)} />
-//         <ExpenseList
-//           expenses={expenses}
-//           onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
-
-//         />
-//       </div>
-//     </>
-//   );
-// };
-
-// export default App;
 import { useEffect, useRef } from "react";
 
 const input = () => {
@@ -69,7 +7,7 @@ const input = () => {
   });
 
   useEffect(() => {
-     document.title = "My first React App";
+     document.title = "My first React App";  
 });
   const inputRef = useRef<HTMLInputElement>(null);
   if (inputRef.current) inputRef.current.focus();
