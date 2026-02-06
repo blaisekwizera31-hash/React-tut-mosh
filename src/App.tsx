b7eed3ff8b8f3900f24d16e115f3ab65992ab8bc
blaisekwizera31-hash <blaisekwizera31@gmail.com>
2026-02-06 07:48:16 +0200

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+
+interface User {
+  id: number;
+  name: string;
+}
 const App = () => {
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState<User[]>([]);
 
   useEffect(() => {
     axios
-      .get("https://jsonplaceholder.typicode.com/users ")
-      .then((res) => console.log(res));
-  });
-  return <div>App</div>;
+      .get<User[]>("https://jsonplaceholder.typicode.com/users ")
+      .then((res) => setUsers(res.data));
+  }, []);
+  return (
+    <div>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+        ,
+      </ul>
+    </div>
+  );
 };
 
 export default App;
