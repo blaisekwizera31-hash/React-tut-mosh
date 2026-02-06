b590dea12a85e4ea6325fbdf05bf2f70d7d60c63
blaisekwizera31-hash <blaisekwizera31@gmail.com>
2026-02-06 10:00:31 +0200

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { CanceledError } from "axios";
 import { useEffect, useState } from "react";
 
 interface User {
@@ -10,11 +10,20 @@ const App = () => {
   const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState([]);
   useEffect(() => {
+    const controller = new AbortController();
+
     axios
-      .get<User[]>("https://jsonplaceholder.typicode.com/users ")
-      .then(res=> setUsers(res.data))
-      .catch(error => setError(error.message));
+      .get<User[]>("https://jsonplaceholder.typicode.com/users ", {
+        signal: controller.signal,
+      })
+      .then((res) => setUsers(res.data))
+      .catch((error) => {
+        if (error instanceof CanceledError) return;
+        setError(error.message);
+      });
     document.title = "first react app";
+
+    return () => controller.abort();
   }, []);
   return (
     <>
