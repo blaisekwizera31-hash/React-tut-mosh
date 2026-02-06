8f664683b3abe8b7fb67def31f8b2c7dab04ce18
blaisekwizera31-hash <blaisekwizera31@gmail.com>
2026-02-06 08:13:48 +0200

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,32 @@ import { useEffect, useState } from "react";
 interface User {
   id: number;
   name: string;
+  email: string;
 }
 const App = () => {
   const [users, setUsers] = useState<User[]>([]);
-
+  const [error, setError] = useState([]);
   useEffect(() => {
     axios
       .get<User[]>("https://jsonplaceholder.typicode.com/users ")
-      .then((res) => setUsers(res.data));
+      .then(res=> setUsers(res.data))
+      .catch(error => setError(error.message));
+    document.title = "first react app";
   }, []);
   return (
-    <div>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-        ,
-      </ul>
-    </div>
+    <>
+      <p className="text-danger">{error}</p>
+      <div>
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+          {users.map((user) => (
+            <li key={user.id}>{user.email}</li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 };
 
