c14e4d0ea00bb0b11d3f601d8726ba987e0b52d2
blaisekwizera31-hash <blaisekwizera31@gmail.com>
2026-02-07 21:35:53 +0200

    commit

--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -1,9 +1,20 @@
-  import apiClient
-   from "./api-client";
+import apiClient from "./api-client";
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+}
 
-   class  UserService{
-      
+class UserService {
+  getAllUsers() {
+    const controller = new AbortController();
+    const request = apiClient.get<User[]>("/users", {
+      signal: controller.signal,
+    });
+    return { request, cancel: () => controller.abort() };
+  }
+}
 
-   }
+export default new UserService();
 
-   export default new UserService();
+
